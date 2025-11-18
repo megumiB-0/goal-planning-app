2e48c30c4a0da07810cd586052de1bb1ccb1d0f1
megumiB-0 <megumi.b.design@gmail.com>
2025-11-18 09:44:15 +0900

    新規作成

---
+++ src/main/java/com/example/goalplanningapp/GoalPlanningAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.goalplanningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalPlanningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalPlanningAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goalplanningapp/GoalPlanningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goalplanningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalPlanningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
