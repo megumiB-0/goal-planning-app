5ce199d472742ed8b1b1e212008bc867883373bc
megumiB-0 <megumi.b.design@gmail.com>
2025-11-24 22:59:43 +0900

    ログイン機能作成

--- src/main/java/com/example/goalplanningapp/repository/UserRepository.java
+++ src/main/java/com/example/goalplanningapp/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.goalplanningapp.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
 
+public interface UserRepository extends JpaRepository<User, Integer> {
+	public User findByEmail(String email);
 }
