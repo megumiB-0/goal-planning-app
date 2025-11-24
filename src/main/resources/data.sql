f4eeb1db34ae840a6d5dc106e8e15c4ce5168030
megumiB-0 <megumi.b.design@gmail.com>
2025-11-24 22:59:43 +0900

    ログイン機能作成

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,5 +6,5 @@ INSERT IGNORE INTO roles (id, name) VALUES (2,'ROLE_ADMIN');
 INSERT IGNORE INTO users
 (name, date_of_birth, gender, email, password, role_id, enabled)
 VALUES
-('Taro Yamada', '1990-05-10', 0, 'taro@example.com', 'password123', 1, 1),
-('Hanako Suzuki', '1993-11-25', 1, 'hanako@example.com', 'securepass456', 2, 1);
+('Taro Yamada', '1990-05-10', 0, 'taro@example.com', '$2a$10$2JNjTwZBwo7fprL2X4sv.OEKqxnVtsVQvuXDkI8xVGix.U3W5B7CO', 1, 1),
+('Hanako Suzuki', '1993-11-25', 1, 'hanako@example.com', '$2a$10$2JNjTwZBwo7fprL2X4sv.OEKqxnVtsVQvuXDkI8xVGix.U3W5B7CO', 2, 1);
