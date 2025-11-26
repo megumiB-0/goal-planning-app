4dda04418b03b207c98d8959728f40bd35dfe6b2
megumiB-0 <megumi.b.design@gmail.com>
2025-11-26 18:33:05 +0900

    会員登録機能作成

--- src/main/java/com/example/goalplanningapp/service/UserService.java
+++ src/main/java/com/example/goalplanningapp/service/UserService.java
@@ -1,5 +1,53 @@
 package com.example.goalplanningapp.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.goalplanningapp.entity.Role;
+import com.example.goalplanningapp.entity.User;
+import com.example.goalplanningapp.form.SignupForm;
+import com.example.goalplanningapp.repository.RoleRepository;
+import com.example.goalplanningapp.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
 public class UserService {
+	private final UserRepository userRepository;
+	private final RoleRepository roleRepository;
+	private final PasswordEncoder passwordEncoder;
+	
+	public UserService(UserRepository userRepository,RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.roleRepository = roleRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	@Transactional
+	public User createUser(SignupForm signupForm) {
+		User user = new User();
+		Role role =roleRepository.findByName("ROLE_GENERAL");
+		
+		user.setName(signupForm.getName());
+		user.setDateOfBirth(signupForm.getDateOfBirth());
+		user.setGender(signupForm.getGender());
+		user.setEmail(signupForm.getEmail());
+		user.setPassword(passwordEncoder.encode(signupForm.getPassword()));
+		user.setRole(role);
+		user.setEnabled(true);
+		
+		return userRepository.save(user);
+		
+		}
+	
+	// メールアドレスが登録すみかどうかチェックする
+	public boolean isEmailRegistrated(String email) {
+		User user = userRepository.findByEmail(email);
+		return user !=null;
+	}
+	// パスワードとパスワード（確認用）が一致するかチェックする
+	public boolean isSamePassword(String password, String passwordCongirmation) {
+		return password.equals(passwordCongirmation);
+	}
 
 }
