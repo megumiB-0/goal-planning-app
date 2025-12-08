442e7d68b3d967c0de3e84a9a118e72b5598405f
megumiB-0 <megumi.b.design@gmail.com>
2025-12-08 14:08:32 +0900

    home画面にカレンダー追加

---
+++ src/main/resources/static/js/calendar.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded',function(){
+	const calendarEl = document.getElementById('calendar');
+	
+	const calendar = new FullCalendar.Calendar(calendarEl,{
+		initialView: 'timeGridWeek',			// １週間の縦型view
+		allDaySlot: false, 						// 終日枠を表示しない
+		nowIndicator: true,						// 現在時刻の赤線
+		selectable: true,						// クリックで選択可能
+//		events: events,							// SpringBootのAPI
+		locale: 'ja', 							// 日本語表記 
+		themeSystem:'bootstrap5',				// BootStrapデザイン
+		editable: true,							// イベントをドラッグで動かせる
+		slotDuration: '00:30:00',				// 15分刻み
+		contentHeight: '350px'
+		
+	});
+	
+	calendar.render();
+
+});
