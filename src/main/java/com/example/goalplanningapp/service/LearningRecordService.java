c133c9bbc2ebfc4bd6430c27d8fd392c9d7783a1
megumiB-0 <megumi.b.design@gmail.com>
2025-12-10 18:43:39 +0900

    カレンダーから実績登録機能の作成

--- src/main/java/com/example/goalplanningapp/service/LearningRecordService.java
+++ src/main/java/com/example/goalplanningapp/service/LearningRecordService.java
@@ -2,31 +2,109 @@ package com.example.goalplanningapp.service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.example.goalplanningapp.dto.LearningRecordDTO;
+import com.example.goalplanningapp.entity.Goal;
+import com.example.goalplanningapp.entity.LearningRecord;
 import com.example.goalplanningapp.entity.User;
 import com.example.goalplanningapp.repository.LearningRecordRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class LearningRecordService {
 	// DI
 	private final LearningRecordRepository learningRecordRepository;
-	private LearningRecordService(LearningRecordRepository learningRecordRepository) {
+	
+	public LearningRecordService(LearningRecordRepository learningRecordRepository) {
 		this.learningRecordRepository = learningRecordRepository;
 	}
 	
-	
-	public void createRecord(LearningRecordDTO dto, User user) {
-		//String →　　日付型へ
+	//POST用
+	public LearningRecord createRecord(LearningRecordDTO dto, User user, Goal goal) {
+		// String →　　日付型へ
 		LocalDate learningDay = LocalDate.parse(dto.getLearningDay());
 		LocalTime startTime = LocalTime.parse(dto.getStartTime());
 		LocalTime endTime = LocalTime.parse(dto.getEndTime());
-
+		//重複チェック
+		if(learningRecordRepository.existsOverlap(user,learningDay,startTime,endTime)) {
+			throw new IllegalStateException("この時間帯はすでに登録されています。");
+		}
+		// 学習時間を分で計算
+		int learningMinutes = calculateMinutes(startTime,endTime);
 		
+		LearningRecord record = new LearningRecord();
+		
+		record.setUser(user);
+		record.setGoal(goal);
+		record.setLearningDay(learningDay);
+		record.setStartTime(startTime);
+		record.setEndTime(endTime);
+		record.setLearningMinutes(learningMinutes);
+		
+		return learningRecordRepository.save(record);
+	}
+
+	//GET用
+	public List<LearningRecord> getRecords(User user) {
+		return learningRecordRepository.findByUser(user);
 	}
 	
 
+	//PUT用
+	@Transactional
+	public void updateRecord(Integer id, LearningRecordDTO dto, User user) {
+		LearningRecord record = learningRecordRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Record not found"));
+		if(!record.getUser().getId().equals(user.getId())) {
+			throw new SecurityException("Not allowed");
+		}
+		//DTO →　LocalDate,LocalTimeへ変換
+		LocalDate learningDay = LocalDate.parse(dto.getLearningDay());
+		LocalTime startTime = LocalTime.parse(dto.getStartTime());
+		LocalTime endTime = LocalTime.parse(dto.getEndTime());
+		//重複チェック（自身のIDは除外）
+		if(learningRecordRepository.existsOverlapExcludingId(user,learningDay,startTime,endTime,id)) {
+			throw new IllegalStateException("この時間帯はすでに登録されています。");
+		}
+		// 学習時間を分で計算
+		int learningMinutes = calculateMinutes(startTime,endTime);
+		//更新
+		record.setLearningDay(learningDay);
+		record.setStartTime(startTime);
+		record.setEndTime(endTime);
+		record.setLearningMinutes(learningMinutes);
+		
+		learningRecordRepository.save(record);
+	}
 
-}
+	//DELETE用
+	@Transactional
+	public void deleteRecord(Integer id, User user) {
+		LearningRecord record = learningRecordRepository.findById(id)
+					.orElseThrow(()-> new IllegalArgumentException("Record not found"));
+		if(!record.getUser().getId().equals(user.getId())) {
+			throw new SecurityException("Not allowed");
+		}
+		//物理削除
+		learningRecordRepository.delete(record);
+	}
+	
+	//共通メソッド（CREATEとPUT）
+	public int calculateMinutes(LocalTime startTime, LocalTime endTime) {
+		int startTotalMinutes = startTime.getHour() * 60 + startTime.getMinute();
+		int endTotalMinutes;
+		// 0:00は24:00として扱う
+		if(endTime.equals(LocalTime.MIDNIGHT)) {
+			endTotalMinutes = 24 * 60; //24:00の代替
+		}else {
+			endTotalMinutes = endTime.getHour() * 60 + endTime.getMinute();
+		}
+		//経過時間（分）を計算
+		return endTotalMinutes - startTotalMinutes;
+		}
+	}
+
--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,20 +1,140 @@
-document.addEventListener('DOMContentLoaded',function(){
+const csrfToken = document.querySelector('meta[name="_csrf"]').content;
+const csrfHeader = document.querySelector('meta[name="_csrf_header"]').content;
+
+
+document.addEventListener('DOMContentLoaded',async function(){
+	//カレンダー描画要素
 	const calendarEl = document.getElementById('calendar');
-	
+	// FullCalendarのインスタンスを作る
 	const calendar = new FullCalendar.Calendar(calendarEl,{
 		initialView: 'timeGridWeek',			// １週間の縦型view
 		allDaySlot: false, 						// 終日枠を表示しない
 		nowIndicator: true,						// 現在時刻の赤線
 		selectable: true,						// クリックで選択可能
-//		events: events,							// SpringBootのAPI
+		selectMirror: true,						// selectを使うには必須
 		locale: 'ja', 							// 日本語表記 
 		themeSystem:'bootstrap5',				// BootStrapデザイン
+		contentHeight: '350px',
+		//slotDuration: '00:30:00',				// 30分刻み
 		editable: true,							// イベントをドラッグで動かせる
-		slotDuration: '00:30:00',				// 30分刻み
-		contentHeight: '350px'
+		//追加
+		eventDurationEditable: true,
+		eventStartEditable: true,
+		unselectAuto: false,
+		selectOverlap: true, 
 		
+		// 初期イベントの読み込み(GET) SpringBootのAPI
+		events: '/api/learning-records/events',
+		
+		/*async function (fetchInfo, successCallback, failureCallback){
+			try{
+				const res = await fetch('/api/learning-records/events');
+				const data = await res.json();
+				//サーバーのレコードをFullCalender形式に変換
+				const events = data.map(record =>({
+					id: record.id,
+					title: record.learningMinutes + "分",
+					start: record.learningDay + "T" + record.startTime,
+					end: record.learningDay + "T" + record.endTime
+				}));
+				//FullCalendarに渡す
+				successCallback(events);
+			}catch(error){
+				failureCallback(error);
+			}
+		},*/
+		
+		// クリックで新規作成（POST）
+		select: async function (info) {
+			const minutes = (new Date(info.end) - new Date(info.start)) / 60000;
+			
+			const body = {
+				learningDay: info.startStr.substring(0,10),
+				startTime: info.startStr.substring(11,16),
+				endTime: info.endStr.substring(11,16),
+				learningMinutes: minutes
+			};
+			
+			const res =await fetch('/api/learning-records/create',{
+				method: 'POST',
+				headers: {"Content-Type": "application/json",
+						   [csrfHeader]: csrfToken
+						  },
+				body: JSON.stringify(body)
+			});
+			
+			//エラーがあれば画面表示
+			if(!res.ok){
+				const err = await res.json(); 	// JSONを取得
+				alert(err.message);				// サーバーからくるmessageを表示
+				return;							// リターン（イベント追加しない）
+			}
+						
+			const saved =await res.json();
+			// カレンダーに即時反映（これにより複数登録可能）
+			calendar.addEvent({
+				id: saved.id,
+				title: body.learningMinutes + "分",
+				start: saved.start,
+				end: saved.end
+			});
+		},
+
+		//イベントクリックで削除(DELETE)(イベントクリックの誤動作防止)
+		eventClick: async function(info){
+			if(!confirm("この記録を削除しますか?")) return;
+			info.jsEvent.preventDefault(); //accidental dragを無効化
+			const id = info.event.id;
+			const res = await fetch(`/api/learning-records/${id}`,{
+				method:'DELETE',
+				headers:{
+					"Content-Type":"application/json",
+					[csrfHeader]: csrfToken
+				}
+			});
+			if(res.ok){
+				info.event.remove();
+			}else{
+				alert("削除に失敗しました。");
+			}
+		},
+
+		
+		//ドラッグで時間変更（PUT）
+		eventDrop: async function (info) {
+			await updateEvent(info);
+		},
+		eventResize: async function(info){
+			await updateEvent(info);
+		}
 	});
 	
 	calendar.render();
+	
+	// 共通の　PUT処理
+	async function updateEvent(info){
+		const id = info.event.id;
+		const body = {
+			learningDay: info.event.startStr.substring(0,10),
+			startTime: info.event.startStr.substring(11,16),
+			endTime: info.event.endStr.substring(11,16),
+			learningMinutes:
+				(new Date(info.event.end) - new Date(info.event.start))/60000
+		};
+		
+		const res = await fetch(`/api/learning-records/${id}`,{
+			method: 'PUT',
+			headers: {"Content-Type": "application/json",
+					  [csrfHeader]: csrfToken},
+			body: JSON.stringify(body)
+		});
+		//エラーがあれば画面表示
+		if(!res.ok){
+			const err = await res.json();
+			alert(err.message);			// エラー表示
+			info.revert();				// FullCalendarのドラッグ変更を元に戻す
+			return;
+		}
 
+	}
 });
