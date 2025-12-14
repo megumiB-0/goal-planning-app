4602f68d3715f13b8846cbda9b944a1ca96c1bca
megumiB-0 <megumi.b.design@gmail.com>
2025-12-14 17:12:40 +0900

    スケジュール用DB、エンティティ作成

---
+++ src/main/java/com/example/goalplanningapp/entity/RoutineSchedule.java
@@ -0,0 +1,60 @@
+package com.example.goalplanningapp.entity;
+
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Table(name = "routine_schedules")
+@Data
+public class RoutineSchedule {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@Column(name = "title", nullable = false)
+	private String title;
+	
+	@Column(name = "start_time", nullable = false)
+	private LocalTime startTime;
+	
+	@Column(name = "end_time", nullable = false)
+	private LocalTime endTime;
+	
+	@Column(name = "effective_from", nullable = false)
+	private LocalDate effectiveFrom;
+	
+	@Column(name = "effective_to")
+	private LocalDate effectiveTo;
+	
+	@Column(name = "created_at", insertable = false, updatable = false)
+	private Timestamp createdAt;
+	
+	@OneToMany(mappedBy = "routineSchedule", fetch=FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	private List<RoutineScheduleDay> routineScheduleDays;
+	
+}
