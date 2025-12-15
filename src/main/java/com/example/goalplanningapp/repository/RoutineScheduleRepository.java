1bd8f43b213949a3002a5400e06f92865002f442
megumiB-0 <megumi.b.design@gmail.com>
2025-12-15 09:27:38 +0900

    ルーティンスケジュール保存　サービス作成

---
+++ src/main/java/com/example/goalplanningapp/repository/RoutineScheduleRepository.java
@@ -0,0 +1,10 @@
+package com.example.goalplanningapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.goalplanningapp.entity.RoutineSchedule;
+
+public interface RoutineScheduleRepository extends JpaRepository <RoutineSchedule, Integer>{
+
+	
+};
